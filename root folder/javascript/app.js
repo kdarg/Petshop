54bae24df4c7b892821f64677e780987cd156a31
Karen Arguello <karuarguello@gmail.com>
2022-02-12 23:24:37 -0300

    estilos

--- root folder/javascript/app.js
+++ root folder/javascript/app.js
@@ -10,9 +10,23 @@ const get_data = async function(){
 
 	let objects = data.response
 
+	
 	return objects
 
 }
+
+const Toast = Swal.mixin({
+	toast: true,
+	position: 'bottom-end',
+	showConfirmButton: false,
+	timer: 3000,
+	timerProgressBar: true,
+
+	didOpen: (toast) => {
+	toast.addEventListener('mouseenter', Swal.stopTimer)
+	toast.addEventListener('mouseleave', Swal.resumeTimer)
+	}
+  })
 
 const objects = await get_data()
 
@@ -37,6 +51,8 @@ const show_local_storage = function(){
 
 show_local_storage()
 
+
+
 const add = function(event){
 	let favorites = JSON.parse(localStorage.getItem('favoritos')) || []
 
@@ -47,13 +63,31 @@ const add = function(event){
 	let favorites_array = [...favorites_set]
 
 	localStorage.setItem('favoritos', JSON.stringify(favorites_array))
-
+console.log('hola');
 	show_local_storage()
+
+	
+	  Toast.fire({
+		icon: 'success',
+		title: 'Articulo agregado'
+	  })
+
+
+
 }
 
 const remove = function(event){
 
+	
+
 	let favorites = JSON.parse(localStorage.getItem('favoritos')) || []
+
+	if(favorites.includes(event)){
+		Toast.fire({
+			icon: 'error',
+			title: 'Articulo eliminado'
+		  })
+	}
 
 	let favorites_set = new Set(favorites)
 
@@ -63,7 +97,9 @@ const remove = function(event){
 
 	localStorage.setItem('favoritos', JSON.stringify(favorites_decrease))
 
+
 	show_local_storage()
+
 
 }
 
@@ -166,4 +202,28 @@ if (page.id == 'contacto'){
 
 
 
+let contadorDate = new Date('may 1, 2022 00:00:00').getTime();
 
+function contadorDown(){
+    let now = new Date().getTime()
+    gap = contadorDate - now
+
+    let segundos = 1000
+    let minutos = segundos * 60
+    let horas = minutos * 60
+    let dia = horas * 24
+
+    let d = Math.floor(gap / (dia))
+    let h = Math.floor((gap % (dia))/(horas))
+    let m = Math.floor((gap % (horas))/(minutos))
+    let s = Math.floor((gap % (minutos))/(segundos))
+
+    document.getElementById('dias').innerHTML = d
+    document.getElementById('horas').innerHTML = h
+    document.getElementById('minutos').innerHTML = m
+    document.getElementById('segundos').innerHTML = s
+}
+
+setInterval( function(){
+    contadorDown()
+},1000)
